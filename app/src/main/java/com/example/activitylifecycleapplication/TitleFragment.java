e4cac1cb72517fb6513a59fe9a84a4359cc2e6d1
Rachana <rachanachaudhari16@gmail.com>
2020-10-07 15:23:38 +0530

    Changes of fragment and stuff

--- app/src/main/java/com/example/activitylifecycleapplication/TitleFragment.java
+++ app/src/main/java/com/example/activitylifecycleapplication/TitleFragment.java
@@ -1,6 +1,5 @@
 package com.example.activitylifecycleapplication;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,19 +8,21 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
+ * Use the {@link TitleFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BlankFragment extends Fragment {
+public class TitleFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
+    private String mParam2;
 
-    public BlankFragment() {
+    public TitleFragment() {
         // Required empty public constructor
     }
 
@@ -30,13 +31,15 @@ public class BlankFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @return A new instance of fragment BlankFragment.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TitleFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1) {
-        BlankFragment fragment = new BlankFragment();
+    public static TitleFragment newInstance(String param1, String param2) {
+        TitleFragment fragment = new TitleFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -46,6 +49,7 @@ public class BlankFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -53,6 +57,6 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.fragment_title, container, false);
     }
 }
